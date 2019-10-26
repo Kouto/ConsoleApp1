7db67e0fa486f06d4f1a68356e98ca08f0248701
Евгений ★ <zhenya-ski@mail.ru>
2019-10-27 00:26:47 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void show(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+        static int factorial(int m)
+        {
+            int s = 1;
+            for (int k=1; k<=m; k++)
+            {
+                s *= m;
+                k++;
+            }
+            return s;
+        }
+
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            show("test");
+            string result = Convert.ToString(factorial(5));
+            show(result);
+            Console.ReadKey();
+        }
+    }
+}
